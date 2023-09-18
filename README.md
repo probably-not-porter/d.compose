4457ee875aa1067be701036c2bcb46a9afc99ebd
Porter Libby <pelibby16@earlham.edu>
2023-09-17 22:06:51 -0400

    update

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # d.compose
  new version yehaw
 
+
 https://www.youtube.com/watch?v=43c-Sm5GMbc
--- Scripts/player-movement.gd
+++ Scripts/player-movement.gd
@@ -1,28 +1,70 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -300.0
+@export var WALKSPEED : float = 80.0
+@export var RUNSPEED : float = 130.0
+@export var JUMP_VELOCITY : float = -200.0
+
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var animationLock : bool = false
+var direction : float = 0.0
+var inAir : bool = false
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	# ========== Physics ===========
+	if is_on_floor():
+		if inAir == true:
+			animated_sprite.play("jump_end")
+		inAir = false
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y += gravity * delta
+		
+		
+	# ========== Jump =============
+	if (Input.is_action_just_pressed("char_jump") or Input.is_action_just_pressed("char_up")) and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		inAir = true
+		animationLock = true
+		animated_sprite.play("jump_start")
+		
+
+	# ========== Walk and run ============
+	direction = Input.get_axis("char_left", "char_right")
+	if direction:
+		if Input.is_action_pressed("char_run"):
+			velocity.x = direction * RUNSPEED
+		else:
+			velocity.x = direction * WALKSPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, WALKSPEED)
 
 	move_and_slide()
+	update_animation()
+
+func update_animation():
+	if not animationLock:
+		if direction != 0:
+			if Input.is_action_pressed("char_run"):
+				animated_sprite.play("run")
+			else:
+				animated_sprite.play("walk")
+		else:
+			animated_sprite.play("idle")
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+				
+			
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprite.animation == "jump_start":
+		animated_sprite.play("jump_normal")
+	if animated_sprite.animation == "jump_end":
+		animationLock = false;
+		
